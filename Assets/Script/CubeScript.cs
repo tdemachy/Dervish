e03f959844cf0c96b898445a004b8d8a88b044d7
Jordan <finalzeroxx@gmail.com>
2020-01-28 15:22:41 +0100

    Hello world

---
+++ Assets/Script/CubeScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * moveSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * moveSpeed);
+    }
+}
