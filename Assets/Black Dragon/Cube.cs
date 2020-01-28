ec8627b58acc9a1f13ef405a5d4e696029df9a40
Marco <annunziatamarco1@gmail.com>
2020-01-28 15:25:33 +0100

    Hello

---
+++ Assets/Black Dragon/Cube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+		Vector3 move = new Vector3();
+
+		if (Input.GetKey(KeyCode.UpArrow))
+
+			move.z += 0.5f;
+
+		if (Input.GetKey(KeyCode.DownArrow))
+
+			move.z -= 0.5f;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+
+			move.x -= 0.5f;
+
+		if (Input.GetKey(KeyCode.RightArrow))
+
+			move.x += 0.5f;
+
+		transform.position += move;
+	}
+}
